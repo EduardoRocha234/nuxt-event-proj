4304e7a13a9f1d568f1e062dbf20516f648c7ca8
Eduardo Aleixo <duduraleixo2005@gmail.com>
2024-09-24 18:51:43 -0300

    fix(toast): set timeout to 3000

--- plugins/toastification.plugin.ts
+++ plugins/toastification.plugin.ts
@@ -3,7 +3,7 @@ import 'vue-toastification/dist/index.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
 	const options: vt.PluginOptions = {
-		timeout: 4500,
+		timeout: 3000,
 		position: vt.POSITION.TOP_RIGHT,
 		transition: 'Vue-Toastification__bounce',
 		draggable: true,
