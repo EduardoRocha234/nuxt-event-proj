2f62ee03ba67cb4493080464db093bcd40377e50
Eduardo Aleixo <duduraleixo2005@gmail.com>
2024-09-29 20:59:36 -0300

    disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import Lara from '@primevue/themes/lara'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
-	devtools: {enabled: true},
+	devtools: {enabled: false},
 	ssr: true,
 	sourcemap: true,
 	serverDir: 'server',
