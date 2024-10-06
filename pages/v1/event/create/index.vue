8407b9551ead2dd6e600a89a11fb1933e5590d2e
Eduardo Aleixo <duduraleixo2005@gmail.com>
2024-10-06 11:54:08 -0300

    refactor(create event): remove coments

--- pages/v1/event/create/index.vue
+++ pages/v1/event/create/index.vue
@@ -6,29 +6,9 @@
 </template>
 
 <script setup lang="ts">
-import type {IEvent} from '~/interfaces'
-
-// import type {IEvent} from '~/interfaces'
-
-const {$api, $toast, $socket} = useNuxtApp()
-const {user} = useUserStore()
-
-const navbarStore = useNavBarStore()
 const footerbarStore = useFooterBarStore()
 
-// const route = useRoute()
-// const eventId = route.params.id
-
-// const userIsInParticipantsList = computed(
-// 	() => !!event?.value?.participants.some((p) => p.userId === user?.userId)
-// )
-
-// const {data: event, refresh} = await useFetch<IEvent>(
-// 	`/api/v1/events/${eventId}`
-// )
-
 onMounted(() => {
-	navbarStore.setSearchBarIsVisible(false)
 	footerbarStore.setFooterBarVisible(false)
 })
 </script>
